8aaa32b7f039e740b5f60e9588b640b1f0c145d4
Love Arvidsson <53595384+Love-A@users.noreply.github.com>
2020-04-16 23:22:11 +0200

    Update Set-HPBiosConfig.ps1

--- Set-HPBiosConfig.ps1
+++ Set-HPBiosConfig.ps1
@@ -33,14 +33,14 @@
     For local or package set:
     ".\BiosConfigUtility64.exe"
 
-.PARAMETER Deploymenttype
+.PARAMETER DeploymentType
     To specify if script is ran in TS or in OS to determine loglocation.
 
 .EXAMPLE 1 When using UNC
-    PS > .\Set-HPBiosConfig.ps1 -BiosConfigUtility "\\SCCMSERVER\SOURCES\OSD\BIOSconfigUtility64.exe" -PasswordFile "Password.bin" -Deploymenttype "OS"
+    PS > .\Set-HPBiosConfig.ps1 -BiosConfigUtility "\\SCCMSERVER\SOURCES\OSD\BIOSconfigUtility64.exe" -PasswordFile "Password.bin" -DeploymentType "OS"
 
 .EXAMPLE 2 Running local or package
-    PS > .\Set-HPBiosConfig.ps1 -BiosConfigUtility ".\BiosConfigUtility64.exe" -PasswordFile "Password.bin" -Deploymenttype "TS"
+    PS > .\Set-HPBiosConfig.ps1 -BiosConfigUtility ".\BiosConfigUtility64.exe" -PasswordFile "Password.bin" -DeploymentType "TS"
 
 
 
@@ -60,6 +60,7 @@
     Version history:
         
         1.0 - (2020-02-24) Script Created
+        1.1 - (2020-04-16) Added some logging and a switch to determine exitcode
 
 #>
 
@@ -92,21 +93,96 @@ param (
 		'OS'{
 		    $LogPath = Join-Path -Path $env:SystemRoot -ChildPath 'Temp'
 		}
-		'TS'{
-		    # Construct TSEnvironment object
-		    try {
-			$TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction Stop
-		    }
-		    catch [System.Exception] {
-			Write-Warning -Message "Unable to construct Microsoft.SMS.TSEnvironment object" ; exit 3
-		    }
+        'TS'{
+            # Construct TSEnvironment object
+            try {
+                $TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction Stop
+            }
+            catch [System.Exception] {
+                Write-Warning -Message "Unable to construct Microsoft.SMS.TSEnvironment object" ; exit 3
+            }
 
-			# Get Logpath
-		    $LogPath = $TSEnvironment.Value("_SMSTSLogPath")
+	        # Get Logpath
+            $LogPath = $TSEnvironment.Value("_SMSTSLogPath")
 
-        	}
-    	}
+        }
+    }
 
 # Run the BiosConfigUtility and set BIOS settings
 
-   (Start-Process $BIOSConfigUtility -ArgumentList /Set:"`"$ComputerSystemModel.txt`"", /cpwdfile:"`"$PasswordFile`"", /verbose -Wait -Passthru -RedirectStandardOutput $LogPath\BiosSettingsUpdate.log -WindowStyle hidden).ExitCode
+  $BiosConfig = (Start-Process $BIOSConfigUtility -ArgumentList /Set:"`"$ComputerSystemModel.txt`"", /cpwdfile:"`"$PasswordFile`"", /verbose -Wait -Passthru -RedirectStandardOutput $LogPath\BiosSettingsUpdate.log -WindowStyle hidden).ExitCode
+  
+# Exit Script based on ExitCode from BCU
+
+  Switch ($Biosconfig){
+       "0"{
+       Write-Output "Bios Config set $ComputerSystemModel. For more information see log at $LogPath\BiosSettingsUpdate.log..." 
+       Exit 0
+       }
+
+       "1" {
+       Write-Warning "Not Supported WMI result code – Setting is not supported on system."
+       Exit 1
+       }
+
+       "2" {
+       Write-Warning "Unknown WMI result code – Operation failed for unknown reason."
+       Exit 2
+       }
+
+       "3" {
+       Write-Warning "Timeout WMI result code – Operation timed out."
+       Exit 3
+       }
+
+       "4" {
+       Write-Warning "Failed WMI result code – Operation failed."
+       Exit 4
+       }
+
+       "5" {
+       Write-Warning "Invalid Parameter WMI result code – A parameter is missing or wrong type."
+       Exit 5
+       }
+
+       "6" {
+       Write-Warning "Access Denied WMI result code – Setting modification failed due to BIOS permissions."
+       Exit 6
+       }
+
+       "10" {
+       Write-Warning "Valid password not provided. BCU was unable to find a valid password on the command-line."
+       Exit 10
+       }
+
+       "11" {
+       Write-Warning "Config file not valid. BCU was unable to locate the configuration file or unable to read the file at the specified path."
+       Exit 11
+       }
+
+       "12" {
+       Write-Warning "First line in config file is not the keyword 'BIOSConfig.' First line in the configuration file must be the word BIOSConfig followed by the file format version, currently 1.0."
+       Exit 12
+       }
+
+       "13" {
+       Write-Warning "Failed to change setting. BCU failed to change one or more settings. For more information see log at $LogPath\BiosSettingsUpdate.log..."
+       Exit 13
+       }
+
+       "14" {
+       Write-Warning "Unable to write to file or system. BCU was unable to connect to HP BIOS WMI. WMI classes are corrupted or the system is not supported."
+       Exit 14
+       }
+
+       "16" {
+       Write-Warning "BCU was unable to connect to HP BIOS WMI. WMI classes are corrupted or the system is not supported."
+       Exit 16
+       }
+
+       "30" {
+       Write-Warning "Password file error. Unable to read or decrypt the password file."
+       Exit 30
+       }
+
+   }
