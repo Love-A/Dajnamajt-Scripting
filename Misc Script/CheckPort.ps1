5e51fe1aa25e72110727d6daf346d7c5e339c2d7
Love Arvidsson <53595384+Love-A@users.noreply.github.com>
2020-11-10 16:07:15 +0100

    Add files via upload

---
+++ Misc Script/CheckPort.ps1
@@ -0,0 +1,28 @@
+﻿# Set Parameters
+Param(
+    [parameter(Mandatory = $true, HelpMessage = "Ange IP eller FQDN till den server som ska testas.")]
+    #[ValidateNotNullOrEmpty()]
+    [String]$Server = "",
+
+    [parameter(Mandatory = $True, HelpMessage = "Ange den Port eller Portar du vill testa.")]
+    #[ValidateNotNullOrEmpty()]
+    [int[]]$Ports = ""
+)
+    # Get Current IPV4 Address
+        $CurrentIPV4 = Get-NetIPAddress -AddressFamily IPv4 -AddressState Preferred -PrefixOrigin Dhcp | Select-Object -ExpandProperty IPAddress
+
+# Test port connections
+    Foreach ($Port in $Ports){
+         Try{
+            $Socket = new-object Net.Sockets.TcpClient
+            $Socket.Connect($Server,$Port)
+            $Test = $Socket.Connected
+                If($Test -eq $true){
+                Write-Output "Connected to $Server over port $Port"
+                }
+            }
+        Catch{"Could not connect to $Server over port $Port"}
+    }
+    
+    # Write IPV4 Address
+        Write-Output "Current client ipv4 addresss $CurrentIPV4"
