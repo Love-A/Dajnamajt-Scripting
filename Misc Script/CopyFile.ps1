f7d83a9801546a45b152ae967fe9d59111721122
Love Arvidsson <53595384+Love-A@users.noreply.github.com>
2020-11-10 16:07:27 +0100

    Add files via upload

---
+++ Misc Script/CopyFile.ps1
@@ -0,0 +1,13 @@
+﻿Param(
+[parameter(Mandatory = $true, HelpMessage = "Ange filen eller katalogen du vill kopiera.")]
+[ValidateNotNullOrEmpty()]
+[String]$FileCopy = "",
+
+[parameter(Mandatory = $true, HelpMessage = "Ange vart du vill kopiera till.")]
+[ValidateNotNullOrEmpty()]
+[String]$FileDestination = ""
+
+)
+
+New-Item -Path $FileDestination -Name $env:COMPUTERNAME -ItemType "Directory"
+Copy-Item $FilePath -Destination $FileDestination\$env:COMPUTERNAME
