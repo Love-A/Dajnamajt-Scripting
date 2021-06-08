c9a115f348fc7d88611f96afda315ebe569fa547
Love Arvidsson <53595384+Love-A@users.noreply.github.com>
2021-06-08 16:51:52 +0200

    Update Set-HPBIOSConfig.ps1

--- Set-HPBIOSConfig/Set-HPBIOSConfig.ps1
+++ Set-HPBIOSConfig/Set-HPBIOSConfig.ps1
@@ -1,6 +1,8 @@
 <#.DESCRIPTION
-    This will use the gathered BIOS config file from HP BiosConfigUtility and set your systems BIOS preferences. A BIOS Settings file can have .REPSET or .txt filextension, if you use .txt be sure to edit the script at Line 61
-    Make sure your settings file is named the same as your Win32_ComputerSystem property Model, eg "HP ZBook 15 G4.REPSET" as the script will call this WMI Class to match up with a settigsfile.
+    This will use the gathered BIOS config file from HP BiosConfigUtility and set your systems BIOS preferences. 
+    A BIOS Settings file can have .REPSET or .txt filextension, if you use .txt be sure to edit the script at Line 61
+    Make sure your settings file is named the same as your Win32_ComputerSystem property Model, 
+    eg "HP ZBook 15 G4.REPSET" as the script will call this WMI Class to match up with a settigsfile.
     
     Script expects these files to be in the same directory as BiosConfigUtility64.exe :
         Passwordfile.bin
