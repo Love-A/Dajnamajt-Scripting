087529507c60646ffbe5147bdb982f33805d5f54
Love Arvidsson <53595384+Love-A@users.noreply.github.com>
2021-06-08 15:54:45 +0200

    Update Set-HPBIOSConfig.ps1

--- Set-HPBIOSConfig/Set-HPBIOSConfig.ps1
+++ Set-HPBIOSConfig/Set-HPBIOSConfig.ps1
@@ -1,3 +1,40 @@
+<#.DESCRIPTION
+    This will use the gathered BIOS config file from HP BiosConfigUtility and set your systems BIOS preferences.
+    Make sure your settings file is named the same as your Win32_ComputerSystem property Model, eg "HP ZBook 15 G4.txt" as the script will call this WMI Class to match up with a settigsfile.
+    
+    Script expects these files to be in the same directory as BiosConfigUtility64.exe :
+        Passwordfile.bin
+        BiosConfigUtility64.exe
+        BIOSSettings.REPSET
+        BCU_ExitCodes.xml
+        
+    Can be made into a package and ran through TS or as application, or even better is to place it on a share accessible from clients, then you wont have to update the package/application-files when making changes in settingsfiles or adding new ones.
+    When running this from a UNC-path, you have to do this post your Apply Networksettings -step, as the TS is run in SYSTEM context.
+    For Offline-media or package, make sure to set the parameters accordingly.
+    
+    To Download HP BIOS Configuration Utility
+    https://ftp.hp.com/pub/caps-softpaq/cmit/HP_BCU.html
+    
+    For more information on the BIOSConfigUtility:
+    http://ftp.hp.com/pub/caps-softpaq/cmit/whitepapers/BIOS_Configuration_Utility_User_Guide.pdf
+
+.PARAMETER PasswordFile
+    Used to define the name of your Password.bin
+    For local or package set:
+    eg "password"
+
+.PARAMETER BCUPath
+    Used to define the path to your BiosConfigUtility.exe directory, which should also contain you password.bin and biossettings.txt -files.
+    If you want to use an UNC path you need a "Connect to network Folder" -step ahead of the script step.
+    Map the folder containing all your settingsfiles, BCU utility and PWD file to eg "O:" and then set the -BCUPath parameter to "O:"
+    For local or package set:
+    ".\BiosConfigUtility64.exe"
+
+.EXAMPLE 1 When using UNC
+    PS > .\Set-HPBiosConfig.ps1 -BCUPath "O:" -PasswordFile "Password" -DeploymentType "OS"
+.EXAMPLE 2 Running local or package
+    PS > .\Set-HPBiosConfig.ps1 -BCUPath ".\BiosConfigUtility64.exe" -PasswordFile "Password" -DeploymentType "TS"
+#>
 param (
     [Parameter(Mandatory = $True)]
     [ValidateNotNullOrEmpty()]
