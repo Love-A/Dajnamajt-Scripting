75d720dc69ebe36caebde6a3b55541ea878c00c4
Love Arvidsson <53595384+Love-A@users.noreply.github.com>
2021-06-15 15:34:45 +0200

    Update Set-HPBIOSConfig.ps1

--- Set-HPBIOSConfig/Set-HPBIOSConfig.ps1
+++ Set-HPBIOSConfig/Set-HPBIOSConfig.ps1
@@ -1,78 +1,51 @@
 <#.DESCRIPTION
     This script will use the gathered BIOS config file from HP BiosConfigUtility and set your systems BIOS preferences. 
-    A BIOS Settings file can have .REPSET or .txt filextension.
+    A BIOS Settings file can have .REPSET or .txt filextension, if you use .txt be sure to edit the script at Line 61.
     Make sure your settings file is named the same as your Win32_ComputerSystem property Model, 
     eg "HP ZBook 15 G4.REPSET" as the script will call this WMI Class to match up with a settigsfile.
     
     Script expects these files to be in the same directory as BiosConfigUtility64.exe :
         Passwordfile.bin
         BiosConfigUtility64.exe
-        BIOSSettings.REPSET (or .txt)
+        BIOSSettings.REPSET
         BCU_ExitCodes.xml
-        
-    The script uses the "config.xml" file to determine the following:
     
-    $BCUPath
-    UNC Path to the Bios Settings and HP BCU utility
-    
-    $PasswordFile
-    The name of your "Password.bin" file
-    
-    $RepsetExt
-    The fileextension of your BCU Config files, eg .repset or .txt
-
     To Download HP BIOS Configuration Utility
     https://ftp.hp.com/pub/caps-softpaq/cmit/HP_BCU.html
     
     For more information on the BIOSConfigUtility:
     http://ftp.hp.com/pub/caps-softpaq/cmit/whitepapers/BIOS_Configuration_Utility_User_Guide.pdf
 
+.PARAMETER PasswordFile
+    Used to define the name of your Password.bin
+    For local or package set:
+    eg "password"
 
-.PARAMETER Config
+.PARAMETER BCUPath
+    Used to define the path to your BiosConfigUtility.exe directory, which should also contain you password.bin, biossettings.REPSET and BCU_ExitCodes.xml -files.
+    If you want to use an UNC path you need a "Connect to network Folder" -step ahead of the script step.
+    Map the folder containing all your settingsfiles, BCU utility and PWD file to eg "O:" and then set the -BCUPath parameter to "O:"
+    For local or package set:
+    ".\"
 
-    Enter the UNCPath to the config.xml file, eg "\\SCCMSERVER\OSD$\Bios Settings\Config.xml"
-
-.EXAMPLE
-\\SCCMSERVER\BIOS$\Set-HPBIOSConfig.ps1 -Config .\config.xml
-
-.EXAMPLE
-\\SCCMSERVER\BIOS$\Set-HPBIOSConfig.ps1 -Config \\SCCMSERVER\OSD$\Bios Settings\config.xml
-
-
-
+.EXAMPLE 1 When using UNC
+    PS > .\Set-HPBiosConfig.ps1 -BCUPath "O:" -PasswordFile "Password"
+.EXAMPLE 2 Running local or package
+    PS > .\Set-HPBiosConfig.ps1 -BCUPath ".\" -PasswordFile "Password"
 #>
-
-
-param(
-    [Parameter(HelpMessage='UNC-Path to XML Configuration File', Mandatory = $True)]
+param (
+    [Parameter(Mandatory = $True)]
     [ValidateNotNullOrEmpty()]
-    [ValidateScript({Test-Path -Path $_ -PathType Leaf})]
-    [ValidatePattern('.xml')]
-    [string]$Config
-    )
+    [String]$PasswordFile,
 
-#Get XML config and BCU ExitCodes
-
-    Try{
-        [XML]$ScriptParam = Get-Content "$Config" -EA Stop
-    }
-    Catch{
-        $_.Exception.Message ; Exit 1    
-    }
-        #Get ScriptConfig
-        $BCUPath = $ScriptParam.Config.HPBCUPath
-        $Passwordfile = $ScriptParam.Config.PWDFileName
-        $RepsetExt = $ScriptParam.Config.FileExt
-
-    Try{
-        [XML]$BCUExitCodes = Get-Content "$BCUPath\BCU_ExitCodes.xml" -EA Stop
-    }
-    Catch{
-        $_.Exception.Message ; Exit 1
-    }
+    [Parameter(Mandatory = $True)]
+    [ValidateNotNullOrEmpty()]
+    [String]$BCUPath
+)
 
 # Gather System preferences
 $ComputerSystemModel = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -ExpandProperty Model
+    Write-OutPut "Determined system model is  $ComputerSystemModel. Trying to find matching settingsfile..."
 
 # Set LogPath
     #Construct TSEnv
@@ -81,43 +54,33 @@ $ComputerSystemModel = Get-CimInstance -ClassName Win32_ComputerSystem | Select-
     #Get Logpath
     $LogPath = $TSEnvironment.Value("_SMSTSLogPath")
 
+#Get ExitCode Info
+[XML]$BCUExitCodes = Get-Content "$BCUPath\BCU_ExitCodes.xml"
+
 # Run BCU and set BIOS Settings
     
     #Set BIOS Variables
-    $REPSETFile = "$BCUPath\$ComputerSystemModel$RepsetExt"
-        Write-OutPut "Trying to find $REPSETFile..."
-
+    $REPSETFile = "$BCUPath\$ComputerSystemModel.REPSET"
     $CurrentPassword = "$BCUPath\$PasswordFile.bin"
-        Write-Output "Password.bin file set as $CurrentPassword set..."
-
     $LogFile = "$LogPath\$ComputerSystemModel.REPSETLog.log"
-        Write-output "Output will be logged to $LogFile..."
 
-        # Set HP BCU arguments
-            $HPBCU = @{
-                FilePath = "$BCUPath\BiosConfigUtility64.exe"
-                ArgumentList = @(
-                    "`"/Set:$REPSETFile`"", `
-                    "`"/cpwdfile:$CurrentPassword`"", `
-                    "`"/verbose`""
-                )
-                wait = $True
-                PassThru = $True
-                RedirectStandardOutput = $LogFile
-                WindowStyle = "Hidden"
-            }
+        # Run HP BCU Utility and set BIOS Config for device
+        $HPBCU = @{
+            FilePath = "$BCUPath\BiosConfigUtility64.exe"
+            ArgumentList = @(
+                "`"/Set:$REPSETFile`"", `
+                "`"/cpwdfile:$CurrentPassword`"", `
+                "`"/verbose`""
+            )
+            wait = $True
+            PassThru = $True
+            RedirectStandardOutput = $LogFile
+            WindowStyle = "Hidden"
+        }
 
-        # Run HP BCU
-            Try{
-                $BCUExitCode = (Start-Process @HPBCU).ExitCode
-                Write-Output "Found BCU & Config, will try to config BIOS..."
-            }
-            Catch{
-                $_.Exception.Message ; Exit 1
-            }
+        $BCUExitCode = (Start-Process @HPBCU).ExitCode
 
-        #On success Exit and write exitmessage
-
+        #Exit and write exitmessage
         $ExitCode = "exit$BCUExitCode"
         $ExitCodeMessage = $BCUExitCodes.exitcodes.$ExitCode
         Write-OutPut "$BCUExitCode - $ExitCodeMessage"
