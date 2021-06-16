26fd00193e0aaa37178013f0c6a003dacac5f4b6
Love Arvidsson <53595384+Love-A@users.noreply.github.com>
2021-06-16 17:06:57 +0200

    Update Set-HPBIOSConfig.ps1

--- Set-HPBIOSConfig/BETA/Set-HPBIOSConfig.ps1
+++ Set-HPBIOSConfig/BETA/Set-HPBIOSConfig.ps1
@@ -39,16 +39,15 @@
 \\SCCMSERVER\BIOS$\Set-HPBIOSConfig.ps1 -Config '\\SCCMSERVER\OSD$\Bios Settings\config.xml'
 
 Updates: 
-2021-06-15 --- A bit of rewrite to use a "config.xml" for Script variables instead of script parameters, to make the script static and the only changes needed is in the config.xml
-               Added some more Try/catch
-               More logging
+2021-06-15 --- *A bit of rewrite to use a "config.xml" for Script variables instead of script parameters, to make the script static and the only changes needed is in the config.xml
+               *Added some more Try/catch
+               *More logging
 
-2021-06-16 --- Added <SetGet> to config.xml file, use this to change if BCU is to "/set" or "/get" BIOS Config. This makes it possible to have one Config for Set and one for Get,
+2021-06-16 --- *Added <SetGet> to config.xml file, use this to change if BCU is to "/set" or "/get" BIOS Config. This makes it possible to have one Config for Set and one for Get,
                 if you would like to store your /get at one place, and your /set files at another.
+               *Added code to check and handle if /Set or /get
 
 #>
-
-
 param(
     [Parameter(HelpMessage='UNC-Path to XML Configuration File', Mandatory = $True)]
     [ValidateNotNullOrEmpty()]
@@ -58,7 +57,6 @@ param(
     )
 
 #Get XML config and BCU ExitCodes
-
     Try{
         [XML]$ScriptParam = Get-Content "$Config" -EA Stop
     }
@@ -70,6 +68,7 @@ param(
         $Passwordfile = $ScriptParam.Config.PWDFileName
         $RepsetExt = $ScriptParam.Config.FileExt
         $SetGet = $ScriptParam.Config.SetGet
+        $GetPath = $ScriptParam.Config.GetPath
 
     Try{
         [XML]$BCUExitCodes = Get-Content "$BCUPath\BCU_ExitCodes.xml" -EA Stop
@@ -79,53 +78,73 @@ param(
     }
 
 # Gather System preferences
-$ComputerSystemModel = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -ExpandProperty Model
+    $ComputerSystemModel = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -ExpandProperty Model
 
-# Set LogPath
-    #Construct TSEnv
-    $TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction Stop
-
-    #Get Logpath
-    $LogPath = $TSEnvironment.Value("_SMSTSLogPath")
-
-# Run BCU and set BIOS Settings
+#Construct TSEnv
+    if($SetGet -eq "set"){
+        Try{
+            $TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction Continue 
+            Write-Output "Task Sequence Variables loaded"
+            }
+        Catch{Write-Output "Task Sequence Variables loaded, assuming run in full OS.."}
+    }
     
-    #Set BIOS Variables
-    $REPSETFile = "$BCUPath\$ComputerSystemModel$RepsetExt"
-        Write-OutPut "Trying to find $REPSETFile..."
+#Set BIOS Variables
+    If($SetGet -eq "Set"){
+            If($TSEnvironment -ne $null){
+                $LogPath = $TSEnvironment.Value("_SMSTSLogPath")
+            }
+            else{
+                $LogPath = "$env:windir\CCM\Logs"
+            }
+        $REPSETFile = "$BCUPath\$ComputerSystemModel$RepsetExt"
+            Write-Output "Trying to find $REPSETFile..."
+        $LogFile = "$LogPath\$ComputerSystemModel.REPSETLog.log"
+            Write-Output "Output will be logged to $LogFile..."
+
+    }
+    Else{
+        $REPSETFile = "$GetPath\$ComputerSystemModel$RepsetExt"
+            Write-Output "Config file will be saved as $REPSETFILE..."
+        $LogFile = "$GetPath\$ComputerSystemModel.REPSETCreate.log"
+            Write-Output "Output will be logged to $LogFile..."
+    }
 
     $CurrentPassword = "$BCUPath\$PasswordFile.bin"
         Write-Output "Password.bin file set as $CurrentPassword set..."
+    
 
-    $LogFile = "$LogPath\$ComputerSystemModel.REPSETLog.log"
-        Write-output "Output will be logged to $LogFile..."
+# Set HP BCU arguments for BCU cmdline
+    $HPBCU = @{
+        FilePath = "$BCUPath\BiosConfigUtility64.exe"
+        ArgumentList = @(
+            "`"/$SetGet`:$REPSETFile`"", `
+            "`"/cpwdfile:$CurrentPassword`"", `
+            "`"/verbose`""
+        )
+        wait = $True
+        PassThru = $True
+        RedirectStandardOutput = $LogFile
+        WindowStyle = "Hidden"
+    }
 
-        # Set HP BCU arguments
-            $HPBCU = @{
-                FilePath = "$BCUPath\BiosConfigUtility64.exe"
-                ArgumentList = @(
-                    "`"/$SetGet`:$REPSETFile`"", `
-                    "`"/cpwdfile:$CurrentPassword`"", `
-                    "`"/verbose`""
-                )
-                wait = $True
-                PassThru = $True
-                RedirectStandardOutput = $LogFile
-                WindowStyle = "Hidden"
-            }
-
-        # Run HP BCU
-            Try{
-                $BCUExitCode = (Start-Process @HPBCU).ExitCode
+# Run HP BCU
+    Try{
+        $BCUExitCode = (Start-Process @HPBCU).ExitCode
+            if($SetGet -eq "set"){
                 Write-Output "Found BCU & Config, will try to config BIOS..."
             }
-            Catch{
-                $_.Exception.Message ; Exit 1
+            else{
+                Write-Output "Creating config file..."
             }
+    }
+    Catch{
+        $_.Exception.Message ; Exit 1
+    }
 
-        #Exit and output exitmessage
+#Exit and write exitmessage
 
-        $ExitCode = "exit$BCUExitCode"
-        $ExitCodeMessage = $BCUExitCodes.exitcodes.$ExitCode
-        Write-OutPut "$BCUExitCode - $ExitCodeMessage"
-        Exit $BCUExitCode
+    $ExitCode = "exit$BCUExitCode"
+    $ExitCodeMessage = $BCUExitCodes.exitcodes.$ExitCode
+    Write-OutPut "$BCUExitCode - $ExitCodeMessage"
+    Exit $BCUExitCode
